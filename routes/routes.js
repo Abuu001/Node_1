a2cd3001378a8a2cd0ba81e4bffb298427ec53e7
Abuu001 <abulugz@gmail.com>
2021-02-16 23:03:00 +0300

    add routing endpoints

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,16 @@
 const router =  require('express').Router()
+const controller=require('../controllers/controllers')
 
-router.get('/',(res,req)=>{
-    
-})
+router.post('/channels',controller.postChannel)
+router.get('/channels',controller.getChannels)
+router.get('/channel',controller.getChannel)
+router.delete('/channel/:id',controller.deleteChannel)
+router.patch('/channel/:id',controller.updateChannel)
+
+router.post('/lessons',controller.postLessons)
+router.get('/lessons',controller.getLessons)
+router.get('/lesson/:id',controller.getLesson)
+router.delete('/lesson/:id',controller.deleteLesson)
+router.patch('/lesson/:id',controller.updateLesson)
 
 module.exports= router;
