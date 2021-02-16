0ca395c345d6da005c4bbc956a5a1a5fb127252f
Abuu001 <abulugz@gmail.com>
2021-02-16 23:02:32 +0300

    add middleware

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const morgan=require('morgan')
 const cors = require('cors')
 
 //  middlewares
+app.use(express.json())
 app.use(morgan('dev'))
 app.use(cors())
 app.use('/v1/api',require('./routes/routes'))
-
 
 const PORT = process.env.PORT || 3004;
 server.listen(PORT,()=>console.log(`Server listening on port ${PORT}`))
