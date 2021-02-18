b0edb54381bd5476ac7a3fa1e0520b30aa044a1e
Abuu001 <abulugz@gmail.com>
2021-02-18 11:46:43 +0300

    md

--- config/db.js
+++ config/db.js
@@ -10,11 +10,11 @@ require('dotenv').config('./config/dev.env')
     // });
     
     const devConfig =`postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-    const prodConfig = process.env.DATABASE_URL ;
+    const prodConfig = process.env.DATABASE_URL || `postgres://akupwqptwjjniv:77d7cb774d2b7efe6cfb154793f51e3936800af86399ae53e1f1d7807a2c830f@ec2-54-225-130-212.compute-1.amazonaws.com:5432/df7nfcuolh9cs`;
  
     const pool = new Pool({  
-        connectionString : process.env.DB_ENVIRONMENT === "production" ? prodConfig : devConfig
-    });
+        connectionString : process.env.NODE_ENV === "production" ? prodConfig : devConfig
+    }); 
   
 pool.connect((err,client)=>{
     if(!err){
