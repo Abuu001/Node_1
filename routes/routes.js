ea703179b553f53c9ef116366cee7fbe8910575c
Abuu001 <abulugz@gmail.com>
2021-02-18 21:57:22 +0300

    add login routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,21 @@
 const router =  require('express').Router()
 const controller=require('../controllers/controllers')
 
-router.post('/messages',controller.postMessage)
-router.get('/messages',controller.getMessages)
-router.get('/message/:id',controller.getMessage)
-router.delete('/message/:id',controller.deleteMessage)
-router.patch('/message/:id',controller.updateMessage)
+router.post('/messages',controller.postMessage);
+router.get('/messages',controller.getMessages);
+router.get('/message/:id',controller.getMessage);
+router.delete('/message/:id',controller.deleteMessage);
+router.patch('/message/:id',controller.updateMessage);
 
-router.post('/lessons',controller.postLessons)
-router.get('/lessons',controller.getLessons)
-router.get('/lesson/:id',controller.getLesson)
-router.delete('/lesson/:id',controller.deleteLesson)
-router.patch('/lesson/:id',controller.updateLesson) 
+router.post('/lessons',controller.postLessons);
+router.get('/lessons',controller.getLessons);
+router.get('/lesson/:id',controller.getLesson);
+router.delete('/lesson/:id',controller.deleteLesson);
+router.patch('/lesson/:id',controller.updateLesson);
 
+router.post('/register/user',controller.addUser);
+router.get('/users',controller.getUsers);
+router.get('/user/:username',controller.getUsername);
+router.post('/user/login',controller.Login);
 
-module.exports= router;
+module.exports= router; 
